8cecb3b1dca37e6b0eb84af95b0edbde783c6422
ycuervob <64037012+ycuervob@users.noreply.github.com>
2023-04-21 16:45:07 -0500

    store setup falta configuracion de fetch con axios{

--- src/store.js
+++ src/store.js
@@ -23,8 +23,20 @@ export const store = createStore({
     },
     setChatsPorAtender(state, chats_por_atender) {
       state.chats_por_atender = chats_por_atender;
+    },
+    setSocket(state, socket) {
+      state.socket = socket;
+    },
+    addChatActuales(state, chat) {
+      state.chats_actuales.push(chat);
+    },
+    addChatAtendidos(state, chat) {
+      state.chats_atendidos.push(chat);
+    },
+    addChatPorAtender(state, chat) {
+      state.chats_por_atender.push(chat);
     }
-  },
+ },
   actions: { //De acá se fechea la data aprovechando que acá se hace asincrono
     setSername({ commit }, sername) {
       commit('setSername', sername);
@@ -37,11 +49,35 @@ export const store = createStore({
     },
     setChatsPorAtender({ commit }, chats_por_atender) {
       commit('setChatsPorAtender', chats_por_atender);
-    }
+    },
+    setSocket({ commit }, socket) {
+      commit('setSocket', socket);
+    },
+    addChatActuales({ commit }, chat) {
+      commit('addChatActuales', chat);
+    },
+    addChatAtendidos({ commit }, chat) {
+      commit('addChatAtendidos', chat);
+    },
+    addChatPorAtender({ commit }, chat) {
+      commit('addChatPorAtender', chat);
+    }    
   },
   getters: {
     getSername(state) {
       return state.sername;
+    },
+    getChatsActuales(state) {
+      return state.chats_actuales;
+    },
+    getChatsAtendidos(state) {
+      return state.chats_atendidos;
+    },
+    getChatsPorAtender(state) {
+      return state.chats_por_atender;
+    },
+    getSocket(state) {
+      return state.socket;
     }
   } 
 })
